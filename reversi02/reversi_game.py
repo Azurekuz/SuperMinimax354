e9484e18b0f300dec8aea12484019baf627b1b3e
Azurekuz <Azurekuz@gmail.com>
2019-10-15 15:04:01 -0400

    initial commit
    Just temporarily posting my project. Will remove once we figure out our problem with our minimaxes, and then we'll have a base to work off of.

---
+++ reversi02/reversi_game.py
@@ -0,0 +1,97 @@
+# Written by Toby Dragon
+
+import copy
+from datetime import datetime
+from reversi_board import ReversiBoard
+from reversi_players import HumanPlayer, RandomComputerPlayer, GreedyComputerPlayer, MinimaxComputerPlayer
+
+
+class ReversiGame:
+
+    def __init__(self, player1, player2, show_status=True, board_size=8, board_filename=None):
+        self.player1 = player1
+        self.player2 = player2
+        self.show_status = show_status
+        if board_filename is None:
+            self.board = ReversiBoard(board_size)
+        else:
+            self.board = ReversiBoard(board_filename=board_filename)
+        self.decision_times = {self.player1.symbol: 0, self.player2.symbol: 0}
+        self.play_game()
+
+    def play_game(self):
+        if self.show_status:
+            self.board.draw_board()
+        while self.board.game_continues():
+            self.play_round()
+        if self.show_status:
+            print("Game over, Final Scores:")
+            print_scores(self.board.calc_scores())
+
+    def play_round(self):
+        start = datetime.now()
+        self.play_move(self.player1)
+        self.decision_times[self.player1.symbol] += (datetime.now()-start).total_seconds()
+        start = datetime.now()
+        self.play_move(self.player2)
+        self.decision_times[self.player2.symbol] += (datetime.now()-start).total_seconds()
+
+    def play_move(self, player):
+        if self.board.calc_valid_moves(player.symbol):
+            chosen_move = player.get_move(copy.deepcopy(self.board))
+            if not self.board.make_move(player.symbol, chosen_move):
+                print("Error: invalid move made")
+            elif self.show_status:
+                self.board.draw_board()
+                print_scores(self.board.calc_scores())
+        elif self.show_status:
+            print(player.symbol, "can't move.")
+
+    def calc_winner(self):
+        scores = self.board.calc_scores()
+        if scores[self.player1.symbol] > scores[self.player2.symbol]:
+            return self.player1.symbol
+        if scores[self.player1.symbol] < scores[self.player2.symbol]:
+            return self.player2.symbol
+        else:
+            return "TIE"
+
+    def get_decision_times(self):
+        return self.decision_times
+
+
+def print_scores(score_map):
+    for symbol in score_map:
+        print(symbol, ":", score_map[symbol], end="\t")
+    print()
+
+
+def compare_players(player1, player2, board_size=8, board_filename=None):
+    game_count_map = {player1.symbol: 0, player2.symbol: 0, "TIE": 0}
+    time_elapsed_map = {player1.symbol: 0, player2.symbol: 0}
+    for i in range(1, 11):
+        if i % 10 == 0:
+            #print(i, "games finished")
+            pass
+        if (i % 2 != 0):
+            game = ReversiGame(player1, player2, show_status=False, board_size=board_size, board_filename=board_filename)
+        elif (i % 2 == 0):
+            game = ReversiGame(player2, player1, show_status=False, board_size=board_size, board_filename=board_filename)
+        game_count_map[game.calc_winner()] += 1
+        decision_times = game.get_decision_times()
+        for symbol in decision_times:
+            time_elapsed_map[symbol] += decision_times[symbol]
+    print(game_count_map)
+    print(time_elapsed_map)
+
+
+def main():
+    ReversiGame(MinimaxComputerPlayer("O", 7, True), HumanPlayer("X")) #board_filename="board4by4nearEnd.json"
+    '''
+    for i in range(0, 1):
+        compare_players(MinimaxComputerPlayer("X", 3, False), MinimaxComputerPlayer("O", 3, True), board_size=8)
+        print()
+    '''
+
+if __name__ == "__main__":
+    main()
---
+++ reversi02/reversi_players03.py
@@ -0,0 +1,144 @@
+# adapted by Toby Dragon from original source code by Al Sweigart, available with creative commons license: https://inventwithpython.com/#donate
+import random
+import copy
+from collections import deque
+
+
+class HumanPlayer:
+
+    def __init__(self, symbol):
+        self.symbol = symbol
+
+    def get_move(self, board):
+        # Let the player type in their move.
+        # Returns the move as [x, y] (or returns the strings 'hints' or 'quit')
+        valid_digits = []
+        for i in range(1, board.get_size()+1):
+            valid_digits.append(str(i))
+        no_valid_move = True
+        while no_valid_move:
+            move = input(self.symbol + ', enter your move:').lower()
+            if len(move) == 2 and move[0] in valid_digits and move[1] in valid_digits:
+                x = int(move[0]) - 1
+                y = int(move[1]) - 1
+                if board.is_valid_move(self.symbol, ( x, y) ):
+                    no_valid_move = False
+                    return [x, y]
+                else:
+                    print('Not a valid move.')
+            else:
+                print('Bad input. Type valid x digit, then the y digit.')
+
+
+class RandomComputerPlayer:
+
+    def __init__(self, symbol):
+        self.symbol = symbol
+
+    def get_move(self, board):
+        return random.choice(board.calc_valid_moves(self.symbol))
+
+class GreedyComputerPlayer:
+
+    def __init__(self, symbol):
+        self.symbol = symbol
+
+    def get_move(self, board):
+
+        #This is the board used to emulate the board state.
+        emuBoard = copy.deepcopy(board);
+        max = [0, self.emulateMove(emuBoard, board.calc_valid_moves(self.symbol)[0])]
+        for i in range(1, len(board.calc_valid_moves(self.symbol))):
+            moveValue = self.emulateMove(emuBoard, board.calc_valid_moves(self.symbol)[i])
+            '''
+            if(self.check_corner(emuBoard, board.calc_valid_moves(self.symbol)[i])):
+                moveValue = moveValue * 1.55
+            elif(self.check_edge(emuBoard, board.calc_valid_moves(self.symbol)[i])):
+                moveValue = moveValue * 1.35
+            '''
+            if moveValue > max[1]:
+                max[0] = i;
+                max[1] = moveValue;
+
+        return board.calc_valid_moves(self.symbol)[max[0]]
+
+    def check_edge(self, board, move):
+        if(move[0] == 0 or move[0] == (board.get_size()-1)):
+            return True
+        elif((move[1] == 0 or move[1] == (board.get_size()-1))):
+            return True
+    def check_corner(self, board,move):
+        if ((move[0] == 0 or move[0] == (board.get_size() - 1)) and (move[1] == 0 or move[1] == (board.get_size()-1))):
+            return True
+
+    def emulateMove(self, board, move):
+        board.make_move(self.symbol, move);
+        return board.calc_scores()[self.symbol]
+
+
+class MinimaxComputerPlayer:
+    def __init__(self, symbol, depth):
+        self.symbol = symbol
+        self.cutoff = depth;
+
+    def get_move(self, board):
+        emuBoard = copy.deepcopy(board)
+        max_val = None
+        valid_moves = board.calc_valid_moves(self.symbol)
+        for i in range(0, len(valid_moves)):
+            cur_eval = self.minimax(emuBoard, self.cutoff, board.get_opponent_symbol(self.symbol), valid_moves[i])
+            if max_val is None:
+                max_val = [cur_eval, valid_moves[i]]
+            elif cur_eval > (max_val[0]):
+                max_val = [cur_eval, valid_moves[i]]
+        return max_val[1]
+
+
+
+    def minimax(self, board, depth, cur_symbol, last_move):
+        emuboard = copy.deepcopy(board)
+        emuboard.make_move(cur_symbol, last_move)
+
+        if depth == 0 or len(board.calc_valid_moves(cur_symbol)) == 0:
+            initVal = emuboard.calc_scores()[cur_symbol]
+            '''
+            if ((last_move[0] == 0 or last_move[0] == (board.get_size() - 1)) and (last_move[1] == 0 or last_move[1] == (board.get_size() - 1))):
+                initVal = initVal * 1.55
+            elif ((last_move[0] == 0 or last_move[0] == (board.get_size() - 1)) and (last_move[1] == 0 or last_move[1] == (board.get_size() - 1))):
+                initVal = initVal * 1.35
+            '''
+            return initVal
+
+        if cur_symbol == self.symbol:
+            max_val = None
+            for move in emuboard.calc_valid_moves(cur_symbol):
+                min_max_val = self.minimax(emuboard, depth - 1, board.get_opponent_symbol(self.symbol), move)
+                if max_val is None:
+                    max_val = min_max_val
+                elif min_max_val is not None:
+                    max_val = self.min_max_eval(max_val, min_max_val, False)
+            return max_val
+        elif cur_symbol != self.symbol:
+            min_val = None
+            for move in board.calc_valid_moves(cur_symbol):
+                min_max_val = self.minimax(emuboard, depth - 1, self.symbol, move)
+                if min_val is None:
+                    min_val = min_max_val
+                elif min_max_val is not None:
+                    min_val = self.min_max_eval(min_val, min_max_val, True)
+            return min_val
+    def min_max_eval(self, val1, val2, get_max):
+        if get_max:
+            if(val1 > val2):
+                return val1
+            else:
+                return val2
+        elif not get_max:
+            if (val1 < val2):
+                return val1
+            else:
+                return val2
+
+    def evaulateState(self, board, move):
+        board.make_move(self.symbol, move)
+        return board.calc_scores()[self.symbol]
