b6bcc8ecabfdb7b46abe863cd15918a7ac5e2b66
Eugene Kuznetsov <Azurekuz@gmail.com>
2019-10-16 13:29:19 -0400

    AllPlayers Added

---
+++ reversi02/all_players.py
@@ -0,0 +1,48 @@
+def get_default_player(symbol):
+    """
+    :returns: a default minimax player that can operate successfully on a given 8x8 board
+    """
+    pass
+
+
+def get_player_a(symbol):
+    """
+    :author:
+    :enchancement:
+    :returns: an enhanced minimax player that can operate successfully on a given 8x8 board
+    """
+    pass
+
+
+def get_player_b(symbol):
+    """
+    :author:
+    :enchancement:
+    :returns: an enhanced minimax player that can operate successfully on a given 8x8 board
+    """
+    pass
+
+
+def get_player_c(symbol):
+    """
+    :author:
+    :enchancement:
+    :returns: an enhanced minimax player that can operate successfully on a given 8x8 board
+    """
+    pass
+
+
+def get_player_d(symbol):
+    """
+    :author:
+    :enchancement:
+    :returns: an enhanced minimax player that can operate successfully on a given 8x8 board
+    """
+    pass
+
+
+def get_combined_player(symbol):
+    """
+    :returns: the best combination of the minimax enhancements that your team can create
+    """
+    pass
