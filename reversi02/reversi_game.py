97ba345d4317afe34c18f829617e6cfa2cb93d9c
Eugene Kuznetsov <Azurekuz@gmail.com>
2019-10-20 20:05:15 -0400

    Commit

--- reversi02/reversi_game.py
+++ reversi02/reversi_game.py
@@ -120,7 +120,7 @@ def get_player_c(symbol):
     :enchancement:
     :returns: an enhanced minimax player that can operate successfully on a given 8x8 board
     """
-    player = eMinimaxComputerPlayer(symbol, 4, False)
+    player = eMinimaxComputerPlayer(symbol, 4, True)
     #player = QuiescentSearch(symbol, 4, True)
     return player;
 
