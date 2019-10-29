8aadba2380581e243db1ba8e6612072c90bf5f5b
slvr611 <orion.gesslein@gmail.com>
2019-10-29 00:33:19 -0400

    Revert "Adjusted Import Statements"
    This reverts commit 67decc41d988ba94e65e665851992db9311a35e0.

--- new_agents/all_players.py
+++ new_agents/all_players.py
@@ -1,4 +1,4 @@
-from new_agents import base_player, alpha_beta_pruning, quiescent_search, lookup_table, transposition_table, mini_mega
+import base_player, alpha_beta_pruning, quiescent_search, lookup_table, transposition_table, mini_mega
 
 def get_default_player(symbol):
     """
